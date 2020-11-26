3e18d96d1a3fb5cf16b855d6aee328fd4f0fb9e6
forumgala98 <forumgala085@gmail.com>
2020-11-26 17:58:36 -0500

    //

--- Twitter/Client.fs
+++ Twitter/Client.fs
@@ -1,2 +1,19 @@
 module Twitter.Client
 
+open System
+open System.Collections
+open System.Runtime.CompilerServices
+open System.Threading.Tasks
+open Akka
+open Akka.Actor
+open Akka.Dispatch.SysMsg
+open System.Collections.Generic
+open Akka.FSharp
+open Akka.Actor
+open System.Diagnostics
+open Akka.Util
+open System.Threading;
+open FSharp.Data
+
+type Simple = JsonProvider<""" { "name":"John", "age":94 } """>
+let simple = Simple.Parse(""" { "name":"Tomas", "age":4 } """)     
