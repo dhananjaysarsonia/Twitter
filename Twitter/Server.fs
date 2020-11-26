300a32218d5902d20cec9b1bcfd14fd3470dcdbc
Dhananjay <dhananjaysarsonia@outlook.com>
2020-11-26 17:53:09 -0500

    JSON

--- Twitter/Server.fs
+++ Twitter/Server.fs
@@ -1,2 +1,20 @@
 module Twitter.Server
 
+//server will have a receiver actor first, which will recieve all the messages and take decisions
+//every function will have it's own actor
+//so actors will be
+
+//Register Account actor-> Registers account and sends back OK message.
+//LoginUser -> user logs in, a random number is shared as repsonse ignore number. Log the metric
+//LogOutUser-> not sure how it will be used right now, but will log the metric
+//MentionsInsert
+//HashTagInsert
+//GetFeed -> will show mentions, tweets I follow
+//GetMyTweet -> will show my tweets-- Will filter mentions too. not sure if actually needed but lets put it
+//Search -> Interesting one-> filter based on hashtag or my mentions 
+
+
+//
+
+
+// 
